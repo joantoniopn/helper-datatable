9cb7e722bead99c34ca730056e9cd4bda37597ad
Joao Neto <joao-antoniopn@live.com>
2022-12-14 11:14:28 -0300

    Update HelperDatatables.js
    Feito diversas melhorias

--- HelperDatatables.js
+++ HelperDatatables.js
@@ -1,3 +1,6 @@
+import {tratarErros} from "./erroRequest";
+import "../../css/carregadores/carregando.css";
+
 class HelperDatatables {
 
     constructor(idTabela, url, divPai, parametros_consulta = {}, serverSide  = true, beforeSend = function () {}) {
@@ -11,9 +14,26 @@ class HelperDatatables {
         this._ordem = [[ 0, "asc" ]];
         this._ordena = false;
         this._divPai = divPai;
+        this._searching = false;
+        this._data_src = "data";
+        this._defineLoader = `<div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>`;
     }
 
-    defineOrdenacao(ordem) { this._ordem = ordem; }
+    defineLoader(loader) {
+        this._defineLoader = loader;
+    }
+
+    podePesquisar(pode) {
+        this._searching = pode;
+    }
+
+    defineDataSrc (_data_src) {
+        this._data_src = data_src;
+    }
+
+    defineOrdenacao(posicao, direcao = "desc") {
+        this._ordem = [[ posicao, direcao ]];
+    }
 
     recuperaOrdem() { return this._ordem; }
 
@@ -51,41 +71,69 @@ class HelperDatatables {
 
     criarLinhas(row, data, dataIndex ) {}
 
+    modificaColunas(row, data) {}
+
+    loadCompleto(settings, json, tabela) {}
+
+    xhrCompleto(retorno, tabela) {
+        let self = this;
+        $(self._divPai).removeClass('hidden');
+        $(self._divPai).show();
+        $(self._idTabela).css("width", "100%");
+        $('html, body').animate({ scrollTop: $(self._divPai).offset().top }, 'slow');
+        tabela.columns.adjust();
+    }
+
     async efetuarConsulta() {
         let self = this;
 
-        if ($.fn.DataTable.isDataTable(self._idTabela)) {
+        $.fn.dataTable.ext.errMode = 'throw';
+
+        if ($.fn.dataTable.isDataTable(self._idTabela) ) {
             $(self._idTabela).DataTable().clear().destroy();
+            if (document.getElementById('cabecalho-fixador') !== null) {
+                document.getElementById('cabecalho-fixador').remove();
+            }
         }
 
         $(self._idTabela).empty();
 
         let tabela = $(self._idTabela).DataTable({
             "processing": true,
-            "searching": false,
+            "searching": this._searching,
             "destroy": true,
+            "deferRender": true,
             "sScrollX": true,
             "pageLength": 20,
             "lengthMenu": [[20, 40, 60, 80, 100], [20, 40, 60, 80, 100]],
             "serverSide": self.serverSide,
+            "initComplete": function( settings, json ) {
+                self.loadCompleto(settings, json, tabela);
+            },
             "ajax": {
                 url : self._url,
                 type: "POST",
+                dataSrc: self._data_src,
                 data: function (d,settings) {
                     self.filtroDatatable(d, settings);
                 },
                 beforeSend: this._beforeSend,
                 error: function (jqXHR, textStatus, errorThrown) {
-                    fecharLoader();
                     self.datatableError(jqXHR, textStatus, errorThrown);
                 },
                 complete: function(retorno) {
-                    $(self._divPai).removeClass('hidden');
-                    $(self._divPai).show();
-                    $(self._idTabela).css("width", "100%");
-                    $('html, body').animate({ scrollTop: $(self._divPai).offset().top }, 'slow');
-                    tabela.columns.adjust();
-                    fecharLoader();
+                    if(parseInt(retorno.status) === 200)
+                    {
+                        self.xhrCompleto(retorno, tabela);
+                    }
+                    else
+                    {
+                        $(self._idTabela).DataTable().clear().destroy();
+                        $(self._divPai).addClass('hidden');
+                        if (document.getElementById('cabecalho-fixador') !== null) {
+                            document.getElementById('cabecalho-fixador').remove();
+                        }
+                    }
                 }
             },
             language: {
@@ -109,6 +157,9 @@ class HelperDatatables {
             columnDefs: self.recuperaConfiguracaoColunas(),
             createdRow: function( row, data, dataIndex ) {
                 self.criarLinhas(row, data, dataIndex);
+            },
+            rowCallback: function (row, data) {
+                self.modificaColunas(row, data);
             }
         });
 
